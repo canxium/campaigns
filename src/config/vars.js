b6a2438e990d2339cfa1df8a17cae3a41b71bd17
Neo Krypt <founder@canxium.org>
2024-01-06 20:03:08 +0700

    add back network

--- src/config/vars.js
+++ src/config/vars.js
@@ -24,6 +24,26 @@ module.exports = Object.freeze({
     hedging           : '6deec32876f9e2c54618ef965fb39c28b285c9e4',
   },
   networks            : [{
+    name              : "ethereum",
+    api               : 'https://api.etherscan.io/api?module=account&action=tokentx&sort=desc&apikey=58F719I6WFJ5T83YMJ5IYWEJGQCUWBH8YG',
+    tokens            : [{
+        address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+        decimal: 6,
+    }, {
+        address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+        decimal: 6,
+    }]
+  }, {
+    name              : "polygon",
+    api               : 'https://api.polygonscan.com/api?module=account&action=tokentx&sort=desc&apikey=YTWJFTPRCBYN7BY5DUJC4IE2D1WD12HNN7',
+    tokens            : [{
+        address: "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
+        decimal: 6,
+    }, {
+        address: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
+        decimal: 6,
+    }]
+  } , {
     name              : "bsc",
     api               : 'https://api.bscscan.com/api?module=account&action=tokentx&sort=desc&apikey=BYDNGMJT1U49ZZ76CKNHT6GAUKCQIK6XWF',
     tokens            : [{
